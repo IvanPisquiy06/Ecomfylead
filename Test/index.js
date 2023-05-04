cfb59778c917ece296f338367f1101ad77883f1c
Ivan Pisquiy <pisquiy22@gmail.com>
2023-05-03 23:11:42 -0600

    Agregando archivos e integracón a Phonexa

--- Test/index.js
+++ Test/index.js
@@ -1,12 +1,10 @@
 /*Getting the values of the form*/
 const form = document.getElementById("ping-post-form");
 /*Setting constants with the URL's and header for the token*/
-const apiUrlPing = "https://test-api.jangl.com/v2/solar/ping";
-const apiUrlPost = "https://test-api.jangl.com/v2/solar/post";
-const apiURLDirect = "https://test-api.jangl.com/v2/solar/direct_post";
-const token = "7f9cdaa692c25448598e7450195274c18da7f57c";
+const apiUrlPing = "https://leads-inst338-client.phonexa.com/ping/";
+const apiUrlPost = "https://leads-inst338-client.phonexa.com/post/";
+const apiURLDirect = "https://leads-inst338-client.phonexa.com/fullpost/";
 const apiHeader = {
-  Authorization: `Token ${token}`,
   "Content-Type": "application/json",
   Accept: "application/json",
 };
@@ -15,90 +13,78 @@ const apiHeader = {
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  var currentDateTime = new Date();
-
   try {
     // Ping Request
     const pingResponse = await fetch(apiUrlPing, {
+      mode: "cors",
       method: "POST",
       headers: apiHeader,
       /*Setting the Json sintax to be received and sent*/
       body: JSON.stringify({
-        meta: {
-          originaly_created: currentDateTime.toLocaleString(),
-          source_id: "1A2B3C4D",
-          offer_id: "13502",
-          lead_id_code: "4xyz78b9-0cdc-43a7-98ea-2b680a5313a2",
-          trusted_form_cert_url: "https://cert.trustedform.com/f886071",
-          user_agent:
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/51.0.2704.103 Safari/537.36",
-          landing_page_url: "https://www.somewebsite.com",
-          tcpa_compliant: Boolean,
-          tcpa_compliant_text:
-            "I agree to receive to be contacted by phone or email.",
-        },
-        contact: {
-          phone_last_four: form.elements.telephone.value.slice(-4),
-          zip_code: form.elements.zip_code.value,
-          ip_address: "10.0.0.1",
-        },
-        data: {
-          best_call_time: "Any Time",
-          own_property: true,
-          purchase_time_frame: form.elements.purchase_time.value,
-          monthly_electric_bill: 400,
-          utility_provider: "Southern California Edison",
-          roof_shade: form.elements.roof_shade.value,
-          property_type: form.elements.property_type.value,
-          credit_rating: form.elements.credit_rating.value,
-        },
+        apiId: "2104CDAB524B4FF0AD65526E286DA5DC",
+        apiPassword: "d27589572",
+        productId: 177,
+        trustedForm: "https://cert.trustedform.com/454a35b802f3e7b63ffabb4efedb7c6ebe67886c",
+        jornayaLeadId: "",
+        tcpa: "YES",
+        tcpaLanguage: "I agree to Terms, Privacy, and consent to solar/home servicers to send marketing prerecorded messages and autodialed calls/texts to my phone number above even if it's on any do not call list. Consent is not a condition of purchase. You can opt-out at any time (see Terms). Message/data rates may apply.",
+        webSiteUrl: "https://gogreenandsave.net/",
+        urlConsent: "https://gogreenandsave.net/",
+        address: form.elements.address.value,
+        zip: form.elements.zip_code.value,
+        city: form.elements.city.value,
+        state: form.elements.state.value,
+        ownHome: "YES",
+        monthlyBill: 150,
+        propertyType: "SINGLE_FAMILY",
+        roofType: "UNSURE_OTHER",
+        roofShade: "NO_SHADE",
+        creditRating: "EXCELLENT",
+        purchaseTimeFrame: "IMMEDIATELY",
+        userIp: "10.0.0.1",
+        projectType: "NEW",
+        solarSystemType: "ELECTRICITY"
       }),
     });
 
     const pingJson = await pingResponse.json();
     console.log(pingJson);
-    const authCode = pingJson.authCode;
+    const promise = pingJson.promise;
+    console.log(promise);
 
     // Post Request
     const postResponse = await fetch(apiUrlPost, {
       method: "POST",
       headers: apiHeader,
       body: JSON.stringify({
-        auth_code: `${authCode}`,
-        meta: {
-          originaly_created: currentDateTime.toLocaleString(),
-          source_id: "1A2B3C4D",
-          offer_id: "13502",
-          lead_id_code: "4xyz78b9-0cdc-43a7-98ea-2b680a5313a2",
-          trusted_form_cert_url: "https://cert.trustedform.com/f886071",
-          user_agent:
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/51.0.2704.103 Safari/537.36",
-          landing_page_url: "https://www.somewebsite.com",
-          tcpa_compliant: Boolean,
-          tcpa_compliant_text:
-            "I agree to receive to be contacted by phone or email.",
-        },
-        contact: {
-          first_name: form.elements.first_name.value,
-          last_name: form.elements.last_name.value,
-          email: form.elements.email.value,
-          phone: form.elements.telephone.value,
-          address: form.elements.address.value,
-          city: form.elements.city.value,
-          state: form.elements.state.value,
-          zip_code: form.elements.zip_code.value,
-          ip_address: "10.0.0.1",
-        },
-        data: {
-          best_call_time: "Any Time",
-          own_property: true,
-          purchase_time_frame: form.elements.purchase_time.value,
-          monthly_electric_bill: 400,
-          utility_provider: "Southern California Edison",
-          roof_shade: form.elements.roof_shade.value,
-          property_type: form.elements.property_type.value,
-          credit_rating: form.elements.credit_rating.value,
-        },
+        auth_code: `${promise}`,
+        apiId: "2104CDAB524B4FF0AD65526E286DA5DC",
+        apiPassword: "d27589572",
+        productId: 177,
+        trustedForm: "https://cert.trustedform.com/454a35b802f3e7b63ffabb4efedb7c6ebe67886c",
+        jornayaLeadId: "",
+        tcpa: "YES",
+        tcpaLanguage: "I agree to Terms, Privacy, and consent to solar/home servicers to send marketing prerecorded messages and autodialed calls/texts to my phone number above even if it's on any do not call list. Consent is not a condition of purchase. You can opt-out at any time (see Terms). Message/data rates may apply.",
+        webSiteUrl: "https://gogreenandsave.net/",
+        urlConsent: "https://gogreenandsave.net/",
+        firstName: form.elements.first_name,
+        lastName: form.elements.last_name,
+        email: form.elements.email,
+        mobilePhone: form.elements.telephone,
+        address: form.elements.address.value,
+        zip: form.elements.zip_code.value,
+        city: form.elements.city.value,
+        state: form.elements.state.value,
+        ownHome: "YES",
+        monthlyBill: 150,
+        propertyType: "SINGLE_FAMILY",
+        roofType: "UNSURE_OTHER",
+        roofShade: "NO_SHADE",
+        creditRating: "EXCELLENT",
+        purchaseTimeFrame: "IMMEDIATELY",
+        userIp: "10.0.0.1",
+        projectType: "NEW",
+        solarSystemType: "ELECTRICITY"
       }),
     });
 
@@ -117,48 +103,4 @@ form.addEventListener("submit", async (event) => {
   } catch (error) {
     console.error(error);
   }
-
-  const DirectPostResponse = await fetch(apiURLDirect, {
-    method: "POST",
-    headers: apiHeader,
-    body: JSON.stringify({
-      meta: {
-        originaly_created: currentDateTime.toLocaleString(),
-        source_id: "1A2B3C4D",
-        offer_id: "13502",
-        lead_id_code: "4xyz78b9-0cdc-43a7-98ea-2b680a5313a2",
-        trusted_form_cert_url: "https://cert.trustedform.com/f886071",
-        user_agent:
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/51.0.2704.103 Safari/537.36",
-        landing_page_url: "https://www.somewebsite.com",
-        tcpa_compliant: Boolean,
-        tcpa_compliant_text:
-          "I agree to receive to be contacted by phone or email.",
-      },
-      contact: {
-        first_name: form.elements.first_name.value,
-        last_name: form.elements.last_name.value,
-        email: form.elements.email.value,
-        phone: form.elements.telephone.value,
-        address: form.elements.address.value,
-        city: form.elements.city.value,
-        state: form.elements.state.value,
-        zip_code: form.elements.zip_code.value,
-        ip_address: "10.0.0.1",
-      },
-      data: {
-        best_call_time: "Any Time",
-        own_property: true,
-        purchase_time_frame: form.elements.purchase_time.value,
-        monthly_electric_bill: 400,
-        utility_provider: "Southern California Edison",
-        roof_shade: form.elements.roof_shade.value,
-        property_type: form.elements.property_type.value,
-        credit_rating: form.elements.credit_rating.value,
-      },
-    }),
-  });
-
-  const DirectJson = await DirectPostResponse.json();
-  console.log(DirectJson);
 });
